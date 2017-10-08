73ca460ea330b570f33bb36d247e3104e68c9f7d
Irmina Kaczmarek <irminakacz@gmail.com>
2017-10-08 21:59:03 +0200

    create Card and Review models
    Create Card and Review models and write unit and integration tests.

--- api/models.py
+++ api/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.utils import timezone
+from datetime import timedelta
 
-# Create your models here.
+class Card(models.Model):
+    front = models.CharField(max_length=200)
+    back = models.CharField(max_length=200)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    interval = models.IntegerField(default=0)
+    easiness_factor = models.FloatField(default=2.5)
+
+    @property
+    def is_due(self):
+        if self.last_review_date():
+            next_review = self.last_review_date() + timedelta(days=self.interval)
+            return next_review <= timezone.now()
+        else:
+            return True
+
+    def last_review_date(self):
+        if self.reviews.all().count() == 0:
+            return None
+        else:
+            return self.reviews.all()[0].review_date
+
+    def review(self, answer_quality):
+        if self.times_reviewed() == 0:
+            self.interval = 0;
+        elif self.times_reviewed() == 1:
+            self.interval = 1;
+        elif self.times_reviewed() == 2:
+            self.interval = 6;
+        else:
+            easiness_factor = self.new_easiness_factor(answer_quality)
+            self.easiness_factor = easiness_factor
+            interval = self.new_interval(easiness_factor)
+            self.interval = interval
+
+    def times_reviewed(self):
+        return self.reviews.all().count()
+
+    def new_easiness_factor(self, answer_quality):
+        new_easiness_factor = self.easiness_factor - 0.8 + 0.28 * answer_quality - 0.02 * answer_quality * answer_quality
+        if new_easiness_factor > 2.5:
+            new_easiness_factor = 2.5
+        if new_easiness_factor < 1.1:
+            new_easiness_factor = 1.1
+        return round(new_easiness_factor, 2)
+
+    def new_interval(self, easiness_factor):
+        return int(self.interval * easiness_factor)
+
+
+class Review(models.Model):
+    card = models.ForeignKey(Card, related_name='reviews', on_delete=models.CASCADE)
+    review_date = models.DateTimeField(auto_now_add=True)
+    answer_quality = models.IntegerField()
+
+    class Meta:
+        ordering = ["review_date"]
--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,77 @@
 from django.test import TestCase
+from api.models import Card, Review
 
-# Create your tests here.
+
+class CardTestCase(TestCase):
+    def setUp(self):
+        Card.objects.create(front="default")
+        Card.objects.create(front="front1", interval=6)
+        Card.objects.create(front="front2", interval=30)
+
+    def test_if_default_last_review_date_is_empty(self):
+        card = Card.objects.get(front="default")
+        self.assertEqual(card.last_review_date(), None)
+
+    def test_if_default_times_reviewed_is_zero(self):
+        card = Card.objects.get(front="default")
+        self.assertEqual(card.times_reviewed(), 0)
+
+    def test_if_new_card_is_due(self):
+        card = Card.objects.get(front="default")
+        self.assertEqual(card.is_due, True)
+
+    def test_if_new_easiness_factor_is_calculated_correctly(self):
+        card = Card.objects.get(front="default")
+        self.assertEqual(card.new_easiness_factor(0), 1.7)
+        self.assertEqual(card.new_easiness_factor(1), 1.96)
+        self.assertEqual(card.new_easiness_factor(2), 2.18)
+        self.assertEqual(card.new_easiness_factor(3), 2.36)
+        self.assertEqual(card.new_easiness_factor(4), 2.5)
+        self.assertEqual(card.new_easiness_factor(5), 2.5)
+
+    def test_if_new_interval_is_calculated_correctly(self):
+        card1 = Card.objects.get(front="front1")
+        card2 = Card.objects.get(front="front2")
+        self.assertEqual(card1.new_interval(1.5), 9)
+        self.assertEqual(card2.new_interval(0.86), 25)
+
+
+class CardReviewIntegrationTestCase(TestCase):
+    def setUp(self):
+        Card.objects.create(front="default")
+
+    def test_if_times_reviewed_updated_after_each_review(self):
+        card = Card.objects.get(front="default")
+        Review.objects.create(card=card, answer_quality=2)
+        self.assertEqual(card.times_reviewed(), 1)
+        Review.objects.create(card=card, answer_quality=3)
+        Review.objects.create(card=card, answer_quality=1)
+        self.assertEqual(card.times_reviewed(), 3)
+
+    def test_if_last_review_date_equals_last_review_date(self):
+        card = Card.objects.get(front="default")
+        review = Review.objects.create(card=card, answer_quality=2)
+        self.assertEqual(card.last_review_date(), review.review_date)
+
+    def test_if_easiness_factor_not_gets_updated_until_third_review(self):
+        card = Card.objects.get(front="default")
+        review = Review.objects.create(card=card, answer_quality=4)
+        card.review(review.answer_quality)
+        review = Review.objects.create(card=card, answer_quality=3)
+        card.review(review.answer_quality)
+        review = Review.objects.create(card=card, answer_quality=1)
+        card.review(review.answer_quality)
+        self.assertNotEqual(card.easiness_factor, 2.5)
+
+    def test_if_interval_gets_updated_by_review(self):
+        card = Card.objects.get(front="default")
+        review = Review.objects.create(card=card, answer_quality=2)
+        card.review(review.answer_quality)
+        self.assertNotEqual(card.interval, 0)
+
+    def test_if_card_is_due_immediately_after_review(self):
+        card = Card.objects.get(front="default")
+        review = Review.objects.create(card=card, answer_quality=4)
+        card.review(review.answer_quality)
+        self.assertEqual(card.is_due, False)
+
